d33764be802f0f9b6215ea7f6e46a2e896ad7830
David <davidfischer081998@gmail.com>
2018-12-18 14:43:33 +0100

    Background - Icon Change - Scroll View - Swipe Refresf

--- app/src/main/java/de/bastian/androidproject/MainActivity.java
+++ app/src/main/java/de/bastian/androidproject/MainActivity.java
@@ -8,10 +8,12 @@ import android.content.pm.PackageManager;
 import android.location.Location;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Handler;
 import android.os.Looper;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityCompat;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
@@ -67,6 +69,9 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.C
     private Long lastUpdate = 0L;
     private int updateFrequency = 120000; // = 120 seconds
 
+    //Swipe Refresh
+    SwipeRefreshLayout swipeLayout;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -101,6 +106,34 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.C
             }
 
         };
+
+        //Swipe Refresh
+        // Getting SwipeContainerLayout
+        swipeLayout = findViewById(R.id.swipe_container);
+        // Adding Listener
+        swipeLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                // Your code here
+                Toast.makeText(getApplicationContext(), "Works!", Toast.LENGTH_LONG).show();
+                // To keep animation for 4 seconds
+                new Handler().postDelayed(new Runnable() {
+                    @Override public void run() {
+                        // Stop animation (This will be after 3 seconds)
+                        swipeLayout.setRefreshing(false);
+                    }
+                }, 4000); // Delay in millis
+            }
+        });
+
+        // Scheme colors for animation
+        swipeLayout.setColorSchemeColors(
+                getResources().getColor(android.R.color.holo_blue_bright),
+                getResources().getColor(android.R.color.holo_green_light),
+                getResources().getColor(android.R.color.holo_orange_light),
+                getResources().getColor(android.R.color.holo_red_light)
+        );
+
 
         //building api client
         //buildGoogleApiClient();
@@ -333,7 +366,7 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.C
                 x1 = touchevent.getX();
                 y1 = touchevent.getY();
                 break;
-            case MotionEvent.ACTION_UP:
+            case MotionEvent.ACTION_MOVE:
                 x2 = touchevent.getX();
                 y2 = touchevent.getY();
 
