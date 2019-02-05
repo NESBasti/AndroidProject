ffbca18d78871b2cca3e2213d4919a1a7b62cbd7
NESBasti <Bas-ti@outlook.com>
2019-02-05 08:55:48 +0100

    fixed bug (widget wasnt updating after period)

--- app/src/main/java/de/bastian/androidproject/WeatherAppWidgetProvider.java
+++ app/src/main/java/de/bastian/androidproject/WeatherAppWidgetProvider.java
@@ -18,18 +18,24 @@ public class WeatherAppWidgetProvider extends AppWidgetProvider {
 
     @Override
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+
         SharedPreferences widgetData = context.getSharedPreferences("WIDGET_DATA", Context.MODE_PRIVATE);
         uniqueId = (int) System.currentTimeMillis();
+
         if ((System.currentTimeMillis() > widgetData.getLong("LAST_UPDATE", 0) + updateFrequency)) {
             Location location = new Location("widget");
             location.setLatitude(widgetData.getFloat("LATITUDE", 0));
             location.setLongitude(widgetData.getFloat("LONGITUDE", 0));
             WidgetUpdateJSON widgetUpdateJSON = new WidgetUpdateJSON(context, location);
             widgetUpdateJSON.run();
+
         }
         for (int appWidgetId : appWidgetIds) {
-            //Intent intent = new Intent(context, MainActivity.class);
-            //PendingIntent pendingIntent = PendingIntent.getActivity(context,0, intent, 0);
+            Intent intent = new Intent(context, WeatherAppWidgetProvider.class);
+            intent.setAction(AppWidgetManager.ACTION_APPWIDGET_UPDATE);
+            intent.putExtra(WIDGET_IDS_KEY, appWidgetIds);
+            PendingIntent pendingIntent = PendingIntent.getBroadcast(context,appWidgetId, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+
 
             Intent serviceIntent = new Intent(context, WidgetUpdateService.class);
             Bundle extras = new Bundle();
@@ -40,10 +46,9 @@ public class WeatherAppWidgetProvider extends AppWidgetProvider {
             extras.putLong("LAST_UPDATE", widgetData.getLong("LAST_UPDATE", 0));
             serviceIntent.putExtras(extras);
             context.startService(serviceIntent);
-            PendingIntent pServiceIntent = PendingIntent.getService(context.getApplicationContext(), uniqueId, serviceIntent, Intent.FILL_IN_ACTION);
 
             RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.weather_widget);
-            views.setOnClickPendingIntent(R.id.widget, pServiceIntent);
+            views.setOnClickPendingIntent(R.id.widget, pendingIntent);
 
             views.setImageViewResource(R.id.widgetIcon, R.drawable.cloud);
 
@@ -60,6 +65,9 @@ public class WeatherAppWidgetProvider extends AppWidgetProvider {
             int[] ids = intent.getExtras().getIntArray(WIDGET_IDS_KEY);
             this.onUpdate(context, AppWidgetManager.getInstance(context), ids);
 
+
         } else super.onReceive(context, intent);
     }
+
+
 }
--- app/src/main/java/de/bastian/androidproject/WidgetUpdateService.java
+++ app/src/main/java/de/bastian/androidproject/WidgetUpdateService.java
@@ -15,29 +15,38 @@ public class WidgetUpdateService extends Service {
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
 
-        AppWidgetManager appWidgetManager = AppWidgetManager.getInstance(this.getApplicationContext());
+
+
+        AppWidgetManager appWidgetManager = AppWidgetManager.getInstance(this);
         Bundle extras = intent.getExtras();
         int[] appWidgetIds = new int[0];
         if (extras != null) {
             appWidgetIds = extras.getIntArray(AppWidgetManager.EXTRA_APPWIDGET_IDS);
         }
 
+
         assert appWidgetIds != null;
         if (appWidgetIds.length > 0) {
             for (int widgetId : appWidgetIds) {
-                RemoteViews remoteViews = new RemoteViews(getPackageName(), R.layout.weather_widget);
+                RemoteViews remoteViews = new RemoteViews(this.getPackageName(), R.layout.weather_widget);
 
                 remoteViews.setTextViewText(R.id.widgetTemp, Integer.toString(extras.getInt("TEMPERATURE", 0)) + "°");
-                remoteViews.setTextViewText(R.id.widgetLocation, extras.getString("LOCATION", ""));
+                remoteViews.setTextViewText(R.id.widgetLocation, extras.getString("LOCATION", "") + " " + widgetId);
                 remoteViews.setTextViewText(R.id.widgetLastUpdate, new SimpleDateFormat("EEE HH:mm", Locale.GERMANY).format(new java.util.Date(extras.getLong("LAST_UPDATE", 0))));
                 remoteViews.setImageViewResource(R.id.widgetIcon, extras.getInt("ICON",0));
 
+
+
                 appWidgetManager.updateAppWidget(widgetId, remoteViews);
+
             }
+
             stopSelf();
         }
         return START_STICKY;
     }
+
+
 
 
     @Override
