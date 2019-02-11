76111db21e0d9681a022a6e3ed737266b33bc871
NESBasti <Bas-ti@outlook.com>
2019-02-11 14:23:53 +0100

    icons switched from png to svg, what to wear icons and text, startforeground updated for android O

--- app/src/main/java/de/bastian/androidproject/WidgetUpdateService.java
+++ app/src/main/java/de/bastian/androidproject/WidgetUpdateService.java
@@ -1,11 +1,16 @@
 package de.bastian.androidproject;
 
 import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.app.Service;
 import android.appwidget.AppWidgetManager;
+import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.IBinder;
+import android.support.v4.app.NotificationCompat;
 import android.widget.RemoteViews;
 
 import java.text.SimpleDateFormat;
@@ -49,7 +54,35 @@ public class WidgetUpdateService extends Service {
 
     @Override
     public void onCreate() {
-        startForeground(1, new Notification());
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            String NOTIFICATION_CHANNEL_ID = "de.bastian.androidproject";
+            String channelName = "My background service";
+            NotificationChannel channel = new NotificationChannel(NOTIFICATION_CHANNEL_ID, channelName, NotificationManager.IMPORTANCE_NONE);
+
+            NotificationManager manager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+            assert manager != null;
+            manager.createNotificationChannel(channel);
+
+            Notification.Builder builder = new Notification.Builder(this, NOTIFICATION_CHANNEL_ID)
+                    .setContentTitle("APPNAME")
+                    .setContentText("foreground service")
+                    .setAutoCancel(true);
+
+            Notification notification = builder.build();
+            startForeground(1, notification);
+
+        } else {
+
+            NotificationCompat.Builder builder = new NotificationCompat.Builder(this)
+                    .setContentTitle("APPNAME")
+                    .setContentText("foreground service")
+                    .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                    .setAutoCancel(true);
+
+            Notification notification = builder.build();
+
+            startForeground(1, notification);
+        }
         super.onCreate();
     }
 
@@ -57,5 +90,7 @@ public class WidgetUpdateService extends Service {
     public IBinder onBind(Intent intent) {
         return null;
     }
+
+
 
 }
